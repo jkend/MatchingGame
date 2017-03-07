6b2772ab55be3a15791b3dba95164b6869238ba3
jkend <jkend@users.noreply.github.com>
2017-03-07 16:30:43 -0500

    pick random congrats messages

--- MatchingGame/CongratsView.swift
+++ MatchingGame/CongratsView.swift
@@ -10,21 +10,36 @@ import UIKit
 
 @IBDesignable
 class CongratsView: UIView {
-
+    // MARK: outlets
     @IBOutlet var view: UIView!
 
+    @IBOutlet weak var congratsTopLabel: UILabel!
     
+    @IBOutlet weak var congratsBottomLabel: UILabel!
     @IBOutlet private weak var timeReportLabel: UILabel!
+    
+    private let congratsTop = ["Yesss!", "W00t!", "Nice!", "Yeah!"]
+    private let congratsBottom = ["Good job!", "You go!", "Awesome!", "You got this."]
+    
     var timeString: String? {
         didSet {
             if timeString != nil {
-                timeReportLabel.text = "You did it in \(timeString!) !"
+                updateLabelText()
             }
         }
     }
     
+    // MARK: update UI
+    private func updateLabelText() {
+        timeReportLabel.text = "You did it in \(timeString!) !"
+        let ind1 = Int(arc4random_uniform(UInt32(congratsTop.count)))
+        let ind2 = Int(arc4random_uniform(UInt32(congratsBottom.count)))
+        congratsTopLabel.text = congratsTop[ind1]
+        congratsBottomLabel.text = congratsBottom[ind2]
+    }
+    
+    // MARK: show and hide
     @IBAction func dismissMe() {
-        //self.isHidden = true
         setHiddenState(true)
     }
     
@@ -33,18 +48,18 @@ class CongratsView: UIView {
     }
     
     private func setHiddenState(_ hidden: Bool) {
-
         UIView.transition(with: self, duration: 0.5, options: .transitionCrossDissolve, animations: {() -> Void in
             self.isHidden = hidden
         }, completion: { _ in })
         
     }
     
+    // MARK: init/load from nib
     override init(frame: CGRect) {
         super.init(frame: frame)
         xibSetup()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         xibSetup()
